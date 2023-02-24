2d6115aa981479a25bfd248e4ac99018454d00b9
Kim Oanh <boanh2910@gmail.com>
2023-02-24 12:35:37 +0700

    add demo

--- DangNhap/Menu.Designer.cs
+++ DangNhap/Menu.Designer.cs
@@ -37,43 +37,43 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 255);
+            this.button1.Location = new System.Drawing.Point(493, 259);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 51);
+            this.button1.Size = new System.Drawing.Size(108, 63);
             this.button1.TabIndex = 0;
             this.button1.Text = "POS bán hàng";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(232, 255);
+            this.button2.Location = new System.Drawing.Point(309, 259);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(89, 51);
+            this.button2.Size = new System.Drawing.Size(108, 63);
             this.button2.TabIndex = 1;
             this.button2.Text = "Quản lí nhân viên";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(838, 255);
+            this.button3.Location = new System.Drawing.Point(894, 259);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(79, 63);
+            this.button3.Size = new System.Drawing.Size(108, 63);
             this.button3.TabIndex = 2;
             this.button3.Text = "Thống kê";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(397, 255);
+            this.button4.Location = new System.Drawing.Point(109, 259);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 51);
+            this.button4.Size = new System.Drawing.Size(108, 63);
             this.button4.TabIndex = 3;
             this.button4.Text = "Quản lí đăng nhập";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(621, 255);
+            this.button5.Location = new System.Drawing.Point(691, 259);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(108, 63);
             this.button5.TabIndex = 4;
